e951d9a1f36902e9d1d5594ef4a8409fe2861e9c
emilyzhang625 <139720917+emilyzhang625@users.noreply.github.com>
2024-09-04 15:25:09 -0700

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,7 @@
-import Profile from "./Profile";
-import Posts from "./Posts";
-import api from "./services/user";
-import { useState, useEffect } from "react";
-import "./App.css";
-import dashboardIcon from "./assets/dashboard.svg";
+import Dashboard from "./Dashboard";
 
 function App() {
-  const [id, setId] = useState(1);
-
-  const [user, setUser] = useState({
-    name: "",
-    username: "",
-    email: "",
-    phone: "",
-    website: "",
-    company: {},
-    address: {},
-  });
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const temp = await api.getUser(id);
-        const ttemp = await api.getPosts();
-        setUser(temp);
-        setPosts(ttemp);
-      } catch (e) {
-        console.log(e);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [id]);
-
-  if (loading)
-    return (
-      <div className="loading-container">
-        <img src={dashboardIcon} className="loader"></img>
-      </div>
-    );
-
-  return (
-    <div className="app-container">
-      <Profile user={user} className="user-side" setId={setId} />
-      <Posts user={user} posts={posts} className="act-side" />
-    </div>
-  );
+  return <Dashboard />;
 }
+
 export default App;
---
+++ src/Dashboard.jsx
@@ -0,0 +1,55 @@
+import Profile from "./Profile";
+import Posts from "./Posts";
+import api from "./services/user";
+import { useState, useEffect } from "react";
+import "./Dashboard.css";
+import dashboardIcon from "./assets/dashboard.svg";
+
+function Dashboard() {
+  const [id, setId] = useState(1);
+
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    website: "",
+    company: {},
+    address: {},
+  });
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const temp = await api.getUser(id);
+        const ttemp = await api.getPosts();
+        setUser(temp);
+        setPosts(ttemp);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  if (loading)
+    return (
+      <div className="loading-container">
+        <img src={dashboardIcon} className="loader"></img>
+      </div>
+    );
+
+  return (
+    <div className="app-container">
+      <Profile user={user} className="user-side" setId={setId} />
+      <Posts user={user} posts={posts} className="act-side" />
+    </div>
+  );
+}
+export default Dashboard;
